a53f84249def767d4a8b9dac563747fffb97daf8
David Neuman <david.m.neuman@gmail.com>
2023-07-18 19:22:50 -0400

    Building PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
 import './App.scss';
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className='App'>
     <div className='photo-list'>
-      {Array(3)
-        .fill()
-        .map((_, i) => (
-          <PhotoListItem key={i} />
-        ))}
+      <PhotoList />
     </div>
   </div>
 );
--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
 
 import '../styles/PhotoList.scss';
+import PhotoListItem from './PhotoListItem';
 
-const PhotoList = () => {
-  <ul className="photo-list">
-    {/* Insert React */}
-  </ul>
-}
+const PhotoList = ({ photos }) => {
+  return (
+    <ul className='photo-list'>
+      {photos.map((photo) => (
+        <PhotoListItem
+          key={photo.id}
+          id={photo.id}
+          username={photo.username}
+          hideUserName={photo.hideUserName}
+          imageSource={photo.imageSource}
+        />
+      ))}
+    </ul>
+  );
+};
 
 PhotoList.defaultProps = {
   photos: [
     {
-     username: 'Jacob',
-     imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
-     id: 1,
-     hideUserName: false,
+      username: 'Jacob',
+      imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
+      id: 1,
+      hideUserName: false,
     },
     {
-     username: 'Jacob',
-     imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
-     id: 2,
-     hideUserName: false,
+      username: 'Jacob',
+      imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
+      id: 2,
+      hideUserName: false,
     },
     {
-     username: 'Jacob',
-     imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
-     id: 3,
-     hideUserName: false,
-    }
-   ]
-}
+      username: 'Jacob',
+      imageSource: `${process.env.PUBLIC_URL}/Image.jpg`,
+      id: 3,
+      hideUserName: false,
+    },
+  ],
+};
 
-export default PhotoList
+export default PhotoList;
