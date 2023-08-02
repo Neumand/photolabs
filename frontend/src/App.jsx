2b741340b7b6f5332ac13431d2337ea7cbc7b882
David Neuman <david.m.neuman@gmail.com>
2023-08-02 19:12:22 -0400

    Create the Top Navigation Bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.scss';
 import PhotoList from './components/PhotoList';
+import TopNavigationBar from './components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className='App'>
+    <TopNavigationBar />
     <div className='photo-list'>
       <PhotoList />
     </div>
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-import '../styles/TopNavigationBar.scss'
+import '../styles/TopNavigationBar.scss';
+import FavBadge from './FavBadge';
+import TopicList from './TopicList';
 
 const TopNavigation = () => {
   return (
-    <div className="top-nav-bar">
-      <span className="top-nav-bar--logo">PhotoLabs</span>
+    <div className='top-nav-bar'>
+      <span className='top-nav-bar--logo'>PhotoLabs</span>
+      <div className='top-nav-bar--topic-list'>
+        <TopicList />
+        <FavBadge />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
